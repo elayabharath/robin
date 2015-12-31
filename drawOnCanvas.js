badfc5c2a49299490dac33f3ebde3756b4d17c0d
elayabharath <elayabharath@semantics3.com>
2016-01-01 03:52:30 +0530

    extracting values of variables from a simple JS program

--- drawOnCanvas.js
+++
@@ -1,5 +0,0 @@
-for(var i = 5; i < 100; i=i+10)
-  for(var j = 5; j < 100; j=j+10) {
-  var myPoint = Point.Create(i*10, j*10);
-  var myCircle = Circle.Create(myPoint, Math.random()*20);
-}
--- loadCanvas.js
+++
@@ -1,5 +0,0 @@
-var canvasElem = document.getElementById('canvas');
-var viewerDiv = document.getElementById('viewer');
-canvasElem.width = viewerDiv.offsetWidth;
-canvasElem.height = viewerDiv.offsetHeight;
-var canvas = canvasElem.getContext("2d");
